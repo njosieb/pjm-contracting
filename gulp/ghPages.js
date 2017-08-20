3d35afeb3aafb6ee820df4744e107c850a5cc2d1
harrisch <charlie.harris@wwt.com>
2017-08-20 14:42:16 -0500

    Adjusts project deployment to use baseUrl

--- gulp/ghPages.js
+++ gulp/ghPages.js
@@ -5,12 +5,10 @@ import path from 'path'
 
 export default function(gulp, plugins, args, config, taskTarget, browserSync) {
   let dirs = config.directories;
-  let dest = path.join(taskTarget);
-  let dataPath = path.join(dirs.source, dirs.data);
 
   gulp.task('ghPages', () => {
-    return gulp.src(`${dest}/**/*`)
-      .pipe(plugins.deploy())
+    return gulp.src(`${dirs.destination}/**/*`)
+      .pipe(plugins.ghPages())
   })
 
 }
